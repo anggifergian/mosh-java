585b1cd4ec411df327042d668519ddf67f34809d
anggifergian <anggyfergian56@gmail.com>
2023-04-08 10:41:36 +0700

    Initial commit - Creating class and object

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Hello World");
+        System.out.println(textBox1.text);
+    }
+}
